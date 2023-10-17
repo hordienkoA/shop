ddf2dfbc97309aad04150095637dd70ec501c81f
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-10-17 18:21:41 +0300

    task5-6-comm

--- src/app/product/resolvers/product.resolver.ts
+++ src/app/product/resolvers/product.resolver.ts
@@ -1,16 +1,14 @@
-import { ResolveFn, Router } from "@angular/router";
+import { ResolveFn } from "@angular/router";
 import { Product } from "../models/product.model";
 import { inject } from "@angular/core";
 import { ProductsPromiseService } from "../services";
 
-export const productResolver: (a: any, b:any)=>Promise<Product> = (route, state)=> {
+export const productResolver: ResolveFn<Product> = (route, state)=> {
   const productService = inject(ProductsPromiseService);
-  const router = inject(Router);
   if(!route.paramMap.has('productID')){
     return Promise.resolve(new Product(null, '','',0,undefined, undefined, undefined));
-
   }
 
   const id = route.paramMap.get('productID')!;
-  return productService.getProduct(id) ?? new Product(null, '','',0,undefined, undefined, undefined);
+  return productService.getProduct(+id) ?? new Product(null, '','',0,undefined, undefined, undefined);
 }
